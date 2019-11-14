241ea745fb66691aeb37fa04584a6817e3997032
asus <asus@asus.com>
2019-11-14 15:22:39 +0800

    test

--- app.js
+++ app.js
@@ -7,44 +7,48 @@ const bodyparser = require('koa-bodyparser')
 const logger = require('koa-logger')
 const sha1 = require('sha1')
 
-const index = require('./routes/index')
-const users = require('./routes/users')
+// const index = require('./routes/index')
+// const users = require('./routes/users')
 
-// error handler
-onerror(app)
+// // error handler
+// onerror(app)
 
-// middlewares
-app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+// // middlewares
+// app.use(bodyparser({
+//   enableTypes:['json', 'form', 'text']
+// }))
+// app.use(json())
+// app.use(logger())
+// app.use(require('koa-static')(__dirname + '/public'))
 
-app.use(views(__dirname + '/views', {
-  extension: 'ejs'
-}))
+// app.use(views(__dirname + '/views', {
+//   extension: 'ejs'
+// }))
 
-// logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// // logger
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+// // routes
+// app.use(index.routes(), index.allowedMethods())
+// app.use(users.routes(), users.allowedMethods())
 
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
-});
+// // error-handling
+// app.on('error', (err, ctx) => {
+//   console.error('server error', err, ctx)
+// });
 app.use(function *(next){
-  console.log(this.query)
+  console.log(this.query + '11111111')
   let token = 'aixiaoxiaozao'
   const { signature, timestamp, nonce, echostr } = this.query
+  console.log('signature =' + signature)
+  console.log('timestamp =' + timestamp)
+  console.log('nonce =' + nonce)
+  console.log('echostr =' + echostr)
   let str = [token, timestamp, nonce].sort().join('')
   let sha = sha1(str)
   console.log(sha)
